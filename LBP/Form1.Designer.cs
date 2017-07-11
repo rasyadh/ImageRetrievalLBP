b85a8dd7035375c8b361a0d6423758a19c341bd5
RASYADH\Rasyadh A Aziz <rasyadhabdulaziz@gmail.com>
2017-07-11 14:35:44 +0700

    first LBP

---
+++ LBP/Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace LBP
+{
+    partial class LBP
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SelectedPic = new System.Windows.Forms.PictureBox();
+            this.BtnLoad = new System.Windows.Forms.Button();
+            this.BtnExtract = new System.Windows.Forms.Button();
+            this.BtnFind = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.OutputPic1 = new System.Windows.Forms.PictureBox();
+            this.OutputPic2 = new System.Windows.Forms.PictureBox();
+            this.OutputPic3 = new System.Windows.Forms.PictureBox();
+            this.OutputPic4 = new System.Windows.Forms.PictureBox();
+            this.OutputPic5 = new System.Windows.Forms.PictureBox();
+            this.OutputPic10 = new System.Windows.Forms.PictureBox();
+            this.OutputPic9 = new System.Windows.Forms.PictureBox();
+            this.OutputPic8 = new System.Windows.Forms.PictureBox();
+            this.OutputPic7 = new System.Windows.Forms.PictureBox();
+            this.OutputPic6 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.SelectedPic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OutputPic1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OutputPic2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OutputPic3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OutputPic4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OutputPic5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OutputPic10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OutputPic9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OutputPic8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OutputPic7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OutputPic6)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(358, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select Image";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(40, 43);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 160);
+            this.listBox1.TabIndex = 41;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // SelectedPic
+            // 
+            this.SelectedPic.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.SelectedPic.Location = new System.Drawing.Point(361, 43);
+            this.SelectedPic.Name = "SelectedPic";
+            this.SelectedPic.Size = new System.Drawing.Size(160, 160);
+            this.SelectedPic.TabIndex = 2;
+            this.SelectedPic.TabStop = false;
+            // 
+            // BtnLoad
+            // 
+            this.BtnLoad.Location = new System.Drawing.Point(198, 71);
+            this.BtnLoad.Name = "BtnLoad";
+            this.BtnLoad.Size = new System.Drawing.Size(125, 30);
+            this.BtnLoad.TabIndex = 3;
+            this.BtnLoad.Text = "Load Image";
+            this.BtnLoad.UseVisualStyleBackColor = true;
+            this.BtnLoad.Click += new System.EventHandler(this.BtnLoad_Click);
+            // 
+            // BtnExtract
+            // 
+            this.BtnExtract.Location = new System.Drawing.Point(198, 107);
+            this.BtnExtract.Name = "BtnExtract";
+            this.BtnExtract.Size = new System.Drawing.Size(125, 30);
+            this.BtnExtract.TabIndex = 4;
+            this.BtnExtract.Text = "Extract Feature";
+            this.BtnExtract.UseVisualStyleBackColor = true;
+            this.BtnExtract.Click += new System.EventHandler(this.BtnExtract_Click);
+            // 
+            // BtnFind
+            // 
+            this.BtnFind.Location = new System.Drawing.Point(198, 143);
+            this.BtnFind.Name = "BtnFind";
+            this.BtnFind.Size = new System.Drawing.Size(125, 30);
+            this.BtnFind.TabIndex = 5;
+            this.BtnFind.Text = "Find Similiar Image";
+            this.BtnFind.UseVisualStyleBackColor = true;
+            this.BtnFind.Click += new System.EventHandler(this.BtnFind_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 229);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Output";
+            // 
+            // OutputPic1
+            // 
+            this.OutputPic1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.OutputPic1.Location = new System.Drawing.Point(40, 248);
+            this.OutputPic1.Name = "OutputPic1";
+            this.OutputPic1.Size = new System.Drawing.Size(140, 140);
+            this.OutputPic1.TabIndex = 7;
+            this.OutputPic1.TabStop = false;
+            // 
+            // OutputPic2
+            // 
+            this.OutputPic2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.OutputPic2.Location = new System.Drawing.Point(186, 248);
+            this.OutputPic2.Name = "OutputPic2";
+            this.OutputPic2.Size = new System.Drawing.Size(140, 140);
+            this.OutputPic2.TabIndex = 8;
+            this.OutputPic2.TabStop = false;
+            // 
+            // OutputPic3
+            // 
+            this.OutputPic3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.OutputPic3.Location = new System.Drawing.Point(332, 248);
+            this.OutputPic3.Name = "OutputPic3";
+            this.OutputPic3.Size = new System.Drawing.Size(140, 140);
+            this.OutputPic3.TabIndex = 9;
+            this.OutputPic3.TabStop = false;
+            // 
+            // OutputPic4
+            // 
+            this.OutputPic4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.OutputPic4.Location = new System.Drawing.Point(478, 248);
+            this.OutputPic4.Name = "OutputPic4";
+            this.OutputPic4.Size = new System.Drawing.Size(140, 140);
+            this.OutputPic4.TabIndex = 10;
+            this.OutputPic4.TabStop = false;
+            // 
+            // OutputPic5
+            // 
+            this.OutputPic5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.OutputPic5.Location = new System.Drawing.Point(624, 248);
+            this.OutputPic5.Name = "OutputPic5";
+            this.OutputPic5.Size = new System.Drawing.Size(140, 140);
+            this.OutputPic5.TabIndex = 11;
+            this.OutputPic5.TabStop = false;
+            // 
+            // OutputPic10
+            // 
+            this.OutputPic10.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.OutputPic10.Location = new System.Drawing.Point(624, 394);
+            this.OutputPic10.Name = "OutputPic10";
+            this.OutputPic10.Size = new System.Drawing.Size(140, 140);
+            this.OutputPic10.TabIndex = 16;
+            this.OutputPic10.TabStop = false;
+            // 
+            // OutputPic9
+            // 
+            this.OutputPic9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.OutputPic9.Location = new System.Drawing.Point(478, 394);
+            this.OutputPic9.Name = "OutputPic9";
+            this.OutputPic9.Size = new System.Drawing.Size(140, 140);
+            this.OutputPic9.TabIndex = 15;
+            this.OutputPic9.TabStop = false;
+            // 
+            // OutputPic8
+            // 
+            this.OutputPic8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.OutputPic8.Location = new System.Drawing.Point(332, 394);
+            this.OutputPic8.Name = "OutputPic8";
+            this.OutputPic8.Size = new System.Drawing.Size(140, 140);
+            this.OutputPic8.TabIndex = 14;
+            this.OutputPic8.TabStop = false;
+            // 
+            // OutputPic7
+            // 
+            this.OutputPic7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.OutputPic7.Location = new System.Drawing.Point(186, 394);
+            this.OutputPic7.Name = "OutputPic7";
+            this.OutputPic7.Size = new System.Drawing.Size(140, 140);
+            this.OutputPic7.TabIndex = 13;
+            this.OutputPic7.TabStop = false;
+            // 
+            // OutputPic6
+            // 
+            this.OutputPic6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.OutputPic6.Location = new System.Drawing.Point(40, 394);
+            this.OutputPic6.Name = "OutputPic6";
+            this.OutputPic6.Size = new System.Drawing.Size(140, 140);
+            this.OutputPic6.TabIndex = 12;
+            this.OutputPic6.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 42;
+            this.label3.Text = "List Image";
+            // 
+            // LBP
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(799, 564);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.OutputPic10);
+            this.Controls.Add(this.OutputPic9);
+            this.Controls.Add(this.OutputPic8);
+            this.Controls.Add(this.OutputPic7);
+            this.Controls.Add(this.OutputPic6);
+            this.Controls.Add(this.OutputPic5);
+            this.Controls.Add(this.OutputPic4);
+            this.Controls.Add(this.OutputPic3);
+            this.Controls.Add(this.OutputPic2);
+            this.Controls.Add(this.OutputPic1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.BtnFind);
+            this.Controls.Add(this.BtnExtract);
+            this.Controls.Add(this.BtnLoad);
+            this.Controls.Add(this.SelectedPic);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "LBP";
+            this.Text = "LBP";
+            ((System.ComponentModel.ISupportInitialize)(this.SelectedPic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OutputPic1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OutputPic2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OutputPic3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OutputPic4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OutputPic5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OutputPic10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OutputPic9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OutputPic8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OutputPic7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OutputPic6)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.PictureBox SelectedPic;
+        private System.Windows.Forms.Button BtnLoad;
+        private System.Windows.Forms.Button BtnExtract;
+        private System.Windows.Forms.Button BtnFind;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox OutputPic1;
+        private System.Windows.Forms.PictureBox OutputPic2;
+        private System.Windows.Forms.PictureBox OutputPic3;
+        private System.Windows.Forms.PictureBox OutputPic4;
+        private System.Windows.Forms.PictureBox OutputPic5;
+        private System.Windows.Forms.PictureBox OutputPic10;
+        private System.Windows.Forms.PictureBox OutputPic9;
+        private System.Windows.Forms.PictureBox OutputPic8;
+        private System.Windows.Forms.PictureBox OutputPic7;
+        private System.Windows.Forms.PictureBox OutputPic6;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ LBP/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LBP
+{
+    public partial class LBP : Form
+    {
+        String selectedPicLBP;
+        int[] loadedPicLBP = new int[256];
+        int[,] dataLBP = new int[572, 256];
+
+        public LBP()
+        {
+            InitializeComponent();
+            for (int i = 1; i < 573; i++)
+                listBox1.Items.Add(i + "-1.png");
+        }
+
+        public int LocalBinaryPattern(int x, int y, Bitmap bitmap)
+        {
+            int count = 0, mindata = 256, tmpdata = 0;
+            int[] data = new int[8];
+            string msg = "";
+            if (bitmap.GetPixel(x, y).R > bitmap.GetPixel(x - 1, y - 1).R) data[0] = 0;
+            else data[0] = 1;
+            if (bitmap.GetPixel(x, y).R > bitmap.GetPixel(x, y - 1).R) data[1] = 0;
+            else data[1] = 1;
+            if (bitmap.GetPixel(x, y).R > bitmap.GetPixel(x + 1, y - 1).R) data[2] = 0;
+            else data[2] = 1;
+            if (bitmap.GetPixel(x, y).R > bitmap.GetPixel(x + 1, y).R) data[3] = 0;
+            else data[3] = 1;
+            if (bitmap.GetPixel(x, y).R > bitmap.GetPixel(x + 1, y + 1).R) data[4] = 0;
+            else data[4] = 1;
+            if (bitmap.GetPixel(x, y).R > bitmap.GetPixel(x, y + 1).R) data[5] = 0;
+            else data[5] = 1;
+            if (bitmap.GetPixel(x, y).R > bitmap.GetPixel(x - 1, y + 1).R) data[6] = 0;
+            else data[6] = 1;
+            if (bitmap.GetPixel(x, y).R > bitmap.GetPixel(x - 1, y).R) data[7] = 0;
+            else data[7] = 1;
+
+            for (int i = 0; i < 8; i++)
+            {
+                tmpdata = Convert(data);
+                if (tmpdata < mindata)
+                    mindata = tmpdata;
+                data = Swap(data);
+            }
+            return mindata;
+        }
+
+        public int Convert(int[] data)
+        {
+            int retval = 0, count = 7;
+            int[] tmp = new int[8];
+            for (int i = 0; i < 8; i++)
+            {
+                tmp[i] = data[count];
+                count--;
+            }
+
+            for (int i = 0; i < 8; i++)
+            {
+                if (tmp[i] == 1)
+                    retval += (int)Math.Pow(2, i);
+            }
+            return retval;
+        }
+
+        public int[] Swap(int[] data)
+        {
+            int tmp = data[0];
+            data[0] = data[1];
+            data[1] = data[2];
+            data[2] = data[3];
+            data[3] = data[4];
+            data[4] = data[5];
+            data[5] = data[6];
+            data[6] = data[7];
+            data[7] = tmp;
+            return data;
+        }
+
+        private void BtnLoad_Click(object sender, EventArgs e)
+        {
+            openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "jpg (*.jpg)|*.jpg|bitmap (*.bitmap)|*.bitmap|png (*.png)|*.png";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK && openFileDialog1.FileName.Length > 0)
+            {
+                SelectedPic.SizeMode = PictureBoxSizeMode.StretchImage;
+                SelectedPic.Image = Image.FromFile(openFileDialog1.FileName);
+                selectedPicLBP = openFileDialog1.FileName;
+            }
+        }
+
+        private void BtnExtract_Click(object sender, EventArgs e)
+        {
+            String path = "F:/Kuliah/Semester 6/Pengolahan Citra/DatasetLBP/";
+            Bitmap bitmap = (Bitmap)SelectedPic.Image;
+            String[] data = new String[256];
+            Color pixelColor;
+            int[] nilaiLBP = new int[256];
+            for (int i = 0; i < 256; i++)
+                nilaiLBP[i] = 0;
+
+            SelectedPic.Image = new Bitmap(SelectedPic.Width, SelectedPic.Height);
+            for (int y = 0; y < bitmap.Height; y++)
+                for (int x = 0; x < bitmap.Width; x++)
+                {
+                    pixelColor = bitmap.GetPixel(x, y);
+                    int red = pixelColor.R;
+                    int green = pixelColor.G;
+                    int blue = pixelColor.B;
+                    int rata = (int)(red + green + blue) / 3;
+                    bitmap.SetPixel(x, y, Color.FromArgb(rata, rata, rata));
+                }
+            
+            SelectedPic.SizeMode = PictureBoxSizeMode.StretchImage;
+            SelectedPic.Image = bitmap;
+            for (int y = 1; y < bitmap.Height - 1; y++)
+                for (int x = 1; x < bitmap.Width - 1; x++)
+                {
+                    int value = LocalBinaryPattern(x, y, bitmap);
+                    nilaiLBP[value] += 1;
+                }
+            
+            loadedPicLBP = nilaiLBP;
+            LoadData();
+        }
+
+        public void LoadData()
+        {
+            string line, msg = "";
+            int count;
+            for (int i = 1; i < 573; i++)
+            {
+                count = 0;
+                System.IO.StreamReader file = new System.IO
+                    .StreamReader(@"F:/Kuliah/Semester 6/Pengolahan Citra/DatasetLBP/" + i + "-1.txt");
+                while ((line = file.ReadLine()) != null)
+                {
+                    dataLBP[i - 1, count] = Int32.Parse(line);
+                    count++;
+                }
+            }
+        }
+
+        private void BtnFind_Click(object sender, EventArgs e)
+        {
+            int selisih;
+            string msg = "";
+            int[] index = new int[572];
+            int[] value = new int[572];
+
+            for (int i = 0; i < 572; i++)
+            {
+                selisih = 0;
+                for (int j = 0; j < 256; j++)
+                    selisih += Math.Abs(loadedPicLBP[j] - dataLBP[i, j]);
+                index[i] = i;
+                value[i] = selisih;
+            }
+
+            for (int i = 0; i < 572; i++)
+                for (int j = 0; j < 571; j++)
+                    if (value[j] > value[j + 1])
+                    {
+                        int tmp = value[j];
+                        value[j] = value[j + 1];
+                        value[j + 1] = tmp;
+
+                        tmp = index[j];
+                        index[j] = index[j + 1];
+                        index[j + 1] = tmp;
+                    }
+
+            OutputPic1.SizeMode = PictureBoxSizeMode.StretchImage;
+            OutputPic1.Image = Image.FromFile("F:/Kuliah/Semester 6/Pengolahan Citra/DatasetLBP/" + (index[1] + 1) + "-1.png");
+
+            OutputPic2.SizeMode = PictureBoxSizeMode.StretchImage;
+            OutputPic2.Image = Image.FromFile("F:/Kuliah/Semester 6/Pengolahan Citra/DatasetLBP/" + (index[2] + 1) + "-1.png");
+
+            OutputPic3.SizeMode = PictureBoxSizeMode.StretchImage;
+            OutputPic3.Image = Image.FromFile("F:/Kuliah/Semester 6/Pengolahan Citra/DatasetLBP/" + (index[3] + 1) + "-1.png");
+
+            OutputPic4.SizeMode = PictureBoxSizeMode.StretchImage;
+            OutputPic4.Image = Image.FromFile("F:/Kuliah/Semester 6/Pengolahan Citra/DatasetLBP/" + (index[4] + 1) + "-1.png");
+
+            OutputPic5.SizeMode = PictureBoxSizeMode.StretchImage;
+            OutputPic5.Image = Image.FromFile("F:/Kuliah/Semester 6/Pengolahan Citra/DatasetLBP/" + (index[5] + 1) + "-1.png");
+
+            OutputPic6.SizeMode = PictureBoxSizeMode.StretchImage;
+            OutputPic6.Image = Image.FromFile("F:/Kuliah/Semester 6/Pengolahan Citra/DatasetLBP/" + (index[6] + 1) + "-1.png");
+
+            OutputPic7.SizeMode = PictureBoxSizeMode.StretchImage;
+            OutputPic7.Image = Image.FromFile("F:/Kuliah/Semester 6/Pengolahan Citra/DatasetLBP/" + (index[7] + 1) + "-1.png");
+
+            OutputPic8.SizeMode = PictureBoxSizeMode.StretchImage;
+            OutputPic8.Image = Image.FromFile("F:/Kuliah/Semester 6/Pengolahan Citra/DatasetLBP/" + (index[8] + 1) + "-1.png");
+
+            OutputPic9.SizeMode = PictureBoxSizeMode.StretchImage;
+            OutputPic9.Image = Image.FromFile("F:/Kuliah/Semester 6/Pengolahan Citra/DatasetLBP/" + (index[9] + 1) + "-1.png");
+
+            OutputPic10.SizeMode = PictureBoxSizeMode.StretchImage;
+            OutputPic10.Image = Image.FromFile("F:/Kuliah/Semester 6/Pengolahan Citra/DatasetLBP/" + (index[10] + 1) + "-1.png");
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SelectedPic.SizeMode = PictureBoxSizeMode.StretchImage;
+            SelectedPic.Image = Image.FromFile("F:/Kuliah/Semester 6/Pengolahan Citra/DatasetLBP/" + (listBox1.SelectedIndex + 1) + "-1.png");
+        }
+        
+    }
+}
